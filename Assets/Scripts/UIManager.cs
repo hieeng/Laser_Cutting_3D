844cf367ce47bedd68e56241d87a88046566f40e
Nam WonHee <q489q489@gmail.com>
2023-02-07 17:08:18 +0900

    UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject startPanel;
+    [SerializeField] GameObject inGamePanel;
+    [SerializeField] GameObject winPanel;
+    [SerializeField] GameObject losePanel;
+
+    [SerializeField] Text gemText;
+    [SerializeField] Text inGameStageText;
+    [SerializeField] Text inGameScoreText;
+    [SerializeField] Text winStageText;
+    [SerializeField] Text winScoreText;
+    [SerializeField] Text loseScoreText;
+    [SerializeField] Text BestScoreText;
+
+    //패널
+    public void OnStartPanel()
     {
-        
+        startPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OffStartPanel()
     {
-        
+        startPanel.SetActive(false);
+    }
+
+    public void OnInGamePanel()
+    {
+        SetInGameStageText();
+        SetInGameScoreText();
+        inGamePanel.SetActive(true);
+    }
+
+    public void OffInGamePanel()
+    {
+        inGamePanel.SetActive(false);
+    }
+
+    public void OnWinPanel()
+    {
+        SetWinGameStageText();
+        SetWinScoreText();
+        winPanel.SetActive(true);
+    }
+
+    public void OffWinPanel()
+    {
+        winPanel.SetActive(false);
+    }
+
+    public void OnLosePanel()
+    {
+        SetLoseScoreText();
+        losePanel.SetActive(true);
+    }
+
+    public void OffLosePanel()
+    {
+        losePanel.SetActive(false);
+    }
+
+    
+    //버튼
+    public void NextButton()
+    {
+        OffWinPanel();
+        OnStartPanel();
+    }
+
+    public void ReStartButton()
+    {
+        OffLosePanel();
+        OnStartPanel();
+    }
+
+    public void ADButton()
+    {
+        //AD ON
+    }
+    
+
+    //텍스트
+    public void SetGemText()
+    {
+        gemText.text = string.Format("{0:n0}", GameManager.Instance.Gem);
+    }
+
+    public void SetInGameStageText()
+    {
+        inGameStageText.text = string.Format("레벨 : {0:n0}", GameManager.Instance.GetCurrentStageLevel());
+    }
+
+    public void SetInGameScoreText()
+    {
+        inGameScoreText.text = string.Format("{0:n0}", GameManager.Instance.Score);
+    }
+
+    public void SetWinGameStageText()
+    {
+        inGameStageText.text = string.Format("레벨 : {0:n0}", GameManager.Instance.GetCurrentStageLevel());
+    }
+
+    public void SetWinScoreText()
+    {
+        winScoreText.text = string.Format("{0:n0}", GameManager.Instance.Score);
+    }
+
+    public void SetLoseScoreText()
+    {
+        loseScoreText.text = string.Format("{0:n0}", GameManager.Instance.Score);
     }
 }
