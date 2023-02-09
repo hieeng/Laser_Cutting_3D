f396f205c810693c5d2c1e956f0b5fa6f3a7b705
Nam WonHee <q489q489@gmail.com>
2023-02-09 14:52:12 +0900

    Gem

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _rewardParticle;
+
+    public void RewardParticle()
+    {
+        _rewardParticle.transform.position = transform.position;
+        _rewardParticle.gameObject.SetActive(true);
+    }
+}
